6b6c6434cae34a11d34a410f2c3e17151147a828
Heytor Cavalcanti <heytor.c.moura@accenture.com>
2022-04-27 21:46:47 -0300

    fix: main content style to fill page

--- src/components/layout/default/index.tsx
+++ src/components/layout/default/index.tsx
@@ -18,7 +18,7 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
 
       <Sidebar />
 
-      <Box component="main" sx={{ flex: 1, flexGrow: 1, p: 3 }}>
+      <Box component="main" sx={{ flex: 1, flexGrow: 1, paddingX: 3, paddingY: 13 }}>
         {children}
       </Box>
     </Box>
