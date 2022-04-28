79830f199849de2b5ee82dae8c6dab9e555663d4
Heytor Cavalcanti <heytor.c.moura@accenture.com>
2022-04-27 21:48:38 -0300

    feat: initial structure to document list

--- src/pages/app/document/index.tsx
+++ src/pages/app/document/index.tsx
@@ -1,41 +1,118 @@
-import React, { useEffect } from 'react'
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import {
+  Paper,
+  Box,
+  Typography as Text,
+  Stack,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Chip,
+} from '@mui/material'
+import { SearchOutlined as SeachIcon, Article as ArticleIcon } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { useDocument } from '@/commons/contexts/document.context';
 
 export default function DocumentPage() {
   const navigate = useNavigate();
-  const { handleDocuments, documents } = useDocument();
 
-  useEffect(() => handleDocuments(), [])
+  const { handleDocuments, documents } = useDocument();
+  
+  const [search, setSearch] = useState<string>('');
+  const [documentList, setDocumentList] = useState(documents)
+
+  useEffect(() => handleDocuments(), []);
+  useEffect(() => setDocumentList(documents), [documents]);
+  useEffect(() => filterDocuments(), [search]);
+
+  const handleSearch = (value: string) => setSearch(value);
+
+  const filterDocuments = () => {
+    if (!search.length) {
+      setDocumentList(documents);
+      return;
+    }
+
+    const documentsFilter = documentList.filter(document => document.name.toLowerCase().includes(search));
+
+    setDocumentList(documentsFilter);
+  }
 
   const goToDocument = (id: string) => {
     navigate(`/app/document/build/${id}`);
   };
 
   return (
-    <Box>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell>Nome</TableCell>
-              <TableCell>Ativo?</TableCell>
-            </TableRow>
-          </TableHead>
+    <>
+      <Stack
+        flex={1}
+        flexDirection="row"
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        mb={5}
+      >
+        <Stack>
+          <Text variant="h5" color="secondary">Documentos</Text>
+        </Stack>
+        <Stack>
+          <FormControl
+            style={{ background: 'white' }}
+            sx={{ m: 1, width: '25ch' }}
+            variant="outlined"
+            size="small"
+          >
+            <InputLabel htmlFor="outlined-adornment-password">Pesquisar</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              value={search}
+              onChange={({ currentTarget: { value } }) => handleSearch(value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                  >
+                    <SeachIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </Stack>
+      </Stack>
 
-          <TableBody>
-            {documents.map(document => (
-              <TableRow key={`document_table_item_${document._id}`} onClick={() => goToDocument(document._id)}>
-                <TableCell>{document._id}</TableCell>
-                <TableCell>{document.name}</TableCell>
-                <TableCell>{document.active}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+      <Box>
+        <Stack flexDirection={'row'}>
+          {documentList.map(document => (
+            <Paper
+              key={`document_table_item_${document._id}`}
+              elevation={3}
+              onClick={() => goToDocument(document._id)}
+            >
+              <Stack
+                flex={1}
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent={'center'}
+                padding={3}
+                sx={{ cursor: 'pointer', width: '13rem', height: '13rem' }}
+              >
+                <ArticleIcon fontSize="large" color="secondary" />
+                <Text marginTop={2} variant={'h6'}>{document.name}</Text>
+                <Text marginBottom={2} variant={'subtitle2'}><small>Versão: {document.version}</small></Text>
+                <Chip
+                  label={document.active ? "Ativo" : "Inativo"}
+                  color={document.active ? "success" : undefined}
+                  variant="outlined"
+                  size="small"
+                />
+              </Stack>
+            </Paper>
+          ))}
+        </Stack>
+      </Box>
+    </>
   )
 }
