b6ecdbcab6ad55caf30862e36a244115fa96b67e
Heytor Cavalcanti <heytor01@live.com>
2022-08-15 22:56:03 -0300

    feat: improve document search performance

--- src/pages/app/document/index.tsx
+++ src/pages/app/document/index.tsx
@@ -40,20 +40,12 @@ export default function DocumentPage() {
 
   useEffect(() => handleDocuments(), []);
   useEffect(() => setDocumentList(documents), [documents]);
-  useEffect(() => filterDocuments(), [search]);
 
   const handleSearch = (value: string) => setSearch(value);
 
-  const filterDocuments = () => {
-    if (!search.length) {
-      setDocumentList(documents);
-      return;
-    }
-
-    const documentsFilter = documentList.filter(document => document.name.toLowerCase().includes(search));
-
-    setDocumentList(documentsFilter);
-  }
+  const filterDocuments = search.length > 0
+    ? documentList.filter(document => document.name.toLowerCase().includes(search))
+    : [];
 
   const goToDocument = (id: string) => {
     navigate(`/app/document/build/${id}`);
@@ -120,32 +112,86 @@ export default function DocumentPage() {
 
       <Box>
         <Stack flexDirection={'row'} gap={3}>
-          {documentList.map(document => (
-            <Paper
-              key={`document_table_item_${document._id}`}
-              elevation={3}
-              onClick={() => goToDocument(document._id)}
-            >
-              <Stack
-                flex={1}
-                flexDirection={'column'}
-                alignItems={'center'}
-                justifyContent={'center'}
-                padding={3}
-                sx={{ cursor: 'pointer', width: '13rem', height: '13rem' }}
-              >
-                <ArticleIcon fontSize="large" color="secondary" />
-                <Text marginTop={2} variant={'h6'}>{document.name}</Text>
-                <Text marginBottom={2} variant={'subtitle2'}><small>Versão: {document.version}</small></Text>
-                <Chip
-                  label={document.active ? "Ativo" : "Inativo"}
-                  color={document.active ? "success" : undefined}
-                  variant="outlined"
-                  size="small"
-                />
-              </Stack>
-            </Paper>
-          ))}
+
+          {filterDocuments.length > 0 ? (
+            <>
+              {filterDocuments.map(document => (
+                <Paper
+                  key={`document_table_item_${document._id}`}
+                  elevation={3}
+                  onClick={() => goToDocument(document._id)}
+                >
+                  <Stack
+                    flex={1}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    padding={3}
+                    sx={{ cursor: 'pointer', width: '15rem', height: '13rem' }}
+                  >
+                    <ArticleIcon fontSize="large" color="secondary" />
+                    <Text
+                      marginTop={2}
+                      variant={'h6'}
+                      align={'center'}
+                      style={{
+                        width: "100%",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >{document.name}</Text>
+                    <Text marginBottom={2} variant={'subtitle2'}><small>Versão: {document.version}</small></Text>
+                    <Chip
+                      label={document.active ? "Ativo" : "Inativo"}
+                      color={document.active ? "success" : undefined}
+                      variant="outlined"
+                      size="small"
+                    />
+                  </Stack>
+                </Paper>
+              ))}
+            </>
+          ) : (
+            <>
+              {documentList.map(document => (
+                <Paper
+                  key={`document_table_item_${document._id}`}
+                  elevation={3}
+                  onClick={() => goToDocument(document._id)}
+                >
+                  <Stack
+                    flex={1}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    padding={3}
+                    sx={{ cursor: 'pointer', width: '15rem', height: '13rem' }}
+                  >
+                    <ArticleIcon fontSize="large" color="secondary" />
+                    <Text
+                      marginTop={2}
+                      variant={'h6'}
+                      align={'center'}
+                      style={{
+                        width: "100%",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >{document.name}</Text>
+                    <Text marginBottom={2} variant={'subtitle2'}><small>Versão: {document.version}</small></Text>
+                    <Chip
+                      label={document.active ? "Ativo" : "Inativo"}
+                      color={document.active ? "success" : undefined}
+                      variant="outlined"
+                      size="small"
+                    />
+                  </Stack>
+                </Paper>
+              ))}
+            </>
+          )}
         </Stack>
       </Box>
 
